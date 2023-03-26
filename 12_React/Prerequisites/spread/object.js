ac15fe6ccf588c7e23e98f2b4a723687126896ef
Shobhitpatel19 <shobhitp49@gmail.com>
2023-03-26 17:03:49 +0530

    destructure

--- 12_React/Prerequisites/spread/object.js
+++ 12_React/Prerequisites/spread/object.js
@@ -10,9 +10,17 @@ let obj = {
 }
 
 // if we do not use spread operator then both the objects will be changed
-let obj2 = {...obj}
+
+//let obj2 = {...obj , address: {...obj.address}, state : {...obj.address.state}}
+
+let obj2 = JSON.parse(JSON.stringify(obj))  //deep copy
 
 obj.name = 'Mark'
+
+// shallow copy
+obj.address.country = 'India'
+obj.address.state.pincode = 22222
+
 
 console.log(obj)
 console.log(obj2)
