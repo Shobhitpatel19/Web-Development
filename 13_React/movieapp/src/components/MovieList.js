55c97bf1762e4164ac2c39d9b08d852deba68d65
Shobhitpatel19 <shobhitp49@gmail.com>
2023-04-22 14:44:46 +0530

    push

--- 13_React/movieapp/src/components/MovieList.js
+++ 13_React/movieapp/src/components/MovieList.js
@@ -1,18 +1,77 @@
 import React, { Component } from "react";
-import { movies } from "../movieData";
+// import { movies } from "../movieData";
+import axios from 'axios';
 
 export class MovieList extends Component {
   constructor() {
+    console.log('constructor first');
     super();
 
     this.state = {
       hover: "",
-      parr : [1]
+      parr : [1],
+      movies : [],
+      currPage : 1
     };
   }
-  render() {
-    let moviesArr = movies.results;
 
+  async componentDidMount(){
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=128ddb90895ab5858a82b040dfcc9888&language=en-US&page=${this.state.currPage}`)
+    let movieData = res.data
+    console.log (movieData);
+    
+    this.setState({
+      movies : [...movieData.results]
+    })
+
+    console.log('mounting done with CDM third');
+  }
+
+  changeMovies = async()=>{
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=128ddb90895ab5858a82b040dfcc9888&language=en-US&page=${this.state.currPage}`)
+    let movieData = res.data
+    console.log (movieData);
+    
+    this.setState({
+      movies : [...movieData.results]
+    })
+  }
+
+  handleNext = ()=>{
+    let tempArr = []
+
+    for(let i=1; i<this.state.parr.length; i++){
+      tempArr.push(i)
+    }
+
+    this.setState({
+      parr:[...tempArr],
+      currPage: this.state.currPage+1
+    })
+
+    this.changeMovies()
+  }
+
+  handlePrevious = () => {
+    if(this.state.currPage != 1){
+      this.setState({
+        currPage : this.state.currPage-1
+      }, this.changeMovies)
+    }
+  }
+
+  handlePageClick = (value)=>{
+     if(value!=this.state.currPage){
+      this.setState({
+        currPage : this.state.currPage-1
+      }, this.changeMovies)
+     }
+  }
+
+  render() {
+    console.log('render second');
+    // let moviesArr = movies.results;
+ 
     return (
       <>
         <div>
@@ -22,7 +81,7 @@ export class MovieList extends Component {
         </div>
 
         <div className="movies-list">
-          {moviesArr.map((movieElem) => (
+          {this.state.movies.map((movieElem) => (
             <div
               className="card movie-card"
               onMouseEnter={() => this.setState({ hover: movieElem.id })} onMouseLeave={() => this.setState({ hover: '' })}
@@ -55,23 +114,23 @@ export class MovieList extends Component {
 
         <div style={{ display: "flex", justifyContent: "center" }}>
           <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item">
-                <a class="page-link" href="#">
+            <ul className="pagination">
+              <li className="page-item">
+                <a className="page-link" onClick={this.handlePrevious}>
                   Previous
                 </a>
               </li>
               {
                 this.state.parr.map((value)=>(
-                  <li class="page-item">
-                <a class="page-link" href="#">
+                  <li className="page-item">
+                <a className="page-link" onClick={()=> this.handlePageClick(value)}>
                   {value}
                 </a>
               </li>
                 ))
               }
-              <li class="page-item">
-                <a class="page-link" href="#">
+              <li className="page-item">
+                <a className="page-link" onClick={this.handleNext}>
                   Next
                 </a>
               </li>
